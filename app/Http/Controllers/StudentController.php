e6714b32bd700fc6446744333c646fdec5a7f33a
UveyisAKBAS <uveysakbas@outlook.com>
2022-12-19 22:21:04 +0300

    Add dynamic components Implement backend to select different students for each request

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -7,11 +7,49 @@ use Illuminate\Support\Facades\Storage;
 
 class StudentController extends Controller
 {
-    public function index()
+    public function index(): array
     {
-        return $this->students();
+        $students = $this->students();
+
+        $previousIndexes = [];
+        if (Storage::disk('local')->exists('indexes.txt')) {
+            $previousIndexes = $this->getPreviousIndexes();
+        }
+
+        $selectedIndexes = $this->selectRandomIndexes($students, $previousIndexes);
+
+        $this->saveSelectedIndexes($selectedIndexes);
+
+
+        return [
+            "totalStudent" => count($students),
+            "students" => collect($students)->only($selectedIndexes)->values()
+        ];
     }
 
+
+    private function selectRandomIndexes(array $students, array $previousIndexes): array
+    {
+        $num = 4;
+
+        $indexes = collect(range(0, count($students) - 1))->except($previousIndexes);
+
+        if ($indexes->count() < $num) {
+            $num = $indexes->count();
+        }
+
+        return $indexes->shuffle()->slice(0, $num)->toArray();
+    }
+
+    private function saveSelectedIndexes(array $indexes)
+    {
+        Storage::disk('local')->put('indexes.txt', json_encode($indexes));
+    }
+
+    private function getPreviousIndexes()
+    {
+        return json_decode(Storage::get('indexes.txt'));
+    }
 
     public function students()
     {
@@ -21,6 +59,10 @@ class StudentController extends Controller
 
     public function seedStudents(int $studentNumber = 30)
     {
+        // https://www.ozyegin.edu.tr/sites/default/files/upload/MuhendislikFakultesi/naz.png
+        // https://www.ozyegin.edu.tr/sites/default/files/upload/MuhendislikFakultesi/taha_0.png
+        // https://www.ozyegin.edu.tr/sites/default/files/upload/MuhendislikFakultesi/deniz.png
+
         $faker = Faker\Factory::create();
 
         $students = [];
